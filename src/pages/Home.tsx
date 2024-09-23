06870d72cb3a39dadb390f95fabde8f7ed204aea
lofi-J <jsj2505@naver.com>
2024-09-23 22:18:46 +0900

    [dev] favicon 설정
     - github profile 이미지 추가
     - profile 파트 내용 작성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { css } from '@emotion/react';
 import {mq} from "../styles/media.ts";
 import { Theme } from '../App.tsx';
 import useTheme from '../hooks/useTheme.ts';
+import githubProfile from '../assets/img/github_profile.png';
 
 
 const Home = () => {
@@ -10,12 +11,29 @@ const Home = () => {
   
   return (
     <main css={main}>
-      <section id={'aboutMe'}>
+      <section id="aboutMe">
         <Character3D />
         <article>
           <div css={greetting(theme)}>
-            <span className={'line1'}>안녕하세요! </span>
-            <span className={'line2'}>Frontend 개발자 <b>조성준</b>입니다.</span>
+            <span className="line1">안녕하세요! </span>
+            <span className="line2">Frontend 개발자 <b>조성준</b>입니다.</span>
+          </div>
+          <div css={profile}>
+            <div className="left">
+              <div className="en_name">Jo Seong Jun</div>
+              <div className="trait">
+                <span>Joyful Coding</span>
+                <span>|</span>
+                <span>Learning Mindset</span>
+              </div>
+            </div>
+            <div className="right">
+              <img src={githubProfile} alt="github_profile" loading={'lazy'} />
+            </div>
+          </div>
+          <div className="description">
+            <div className="iLove">I ♥ React.js, Typescript</div>
+            만들고 싶은 프로젝트를 만들며 성장하는 사람입니다.
           </div>
         </article>
       </section>
@@ -39,6 +57,9 @@ const main = css`
     /* 3D 오브젝트 Canvas 사이즈와 함께 margin-top이 조정되어야 함 */
     margin: 35rem auto 0;
     padding: 0 1.6rem 0;
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
   }
   ${mq('tablet')} {
     #aboutMe {
@@ -58,8 +79,8 @@ const main = css`
 `;
 
 const greetting = (theme: Theme) => css`
-  padding: 0.8rem;
-  border-radius: 0.8rem;
+  padding: 1.2rem 0.8rem;
+  border-radius: 8px;
   background-color: ${theme === 'dark' ? 'var(--white-alpha-200)' : 'var(--white-alpha-500)'};
   text-align: center;
   font-size: 1.6rem;
@@ -77,7 +98,51 @@ const greetting = (theme: Theme) => css`
     display: flex;
     flex-direction: column;
   }
-  
+`;
+
+const profile = css`
+  display: flex;
+  justify-content: space-between;
+  .left {
+    .en_name {
+      font-size: 3.6rem;
+      font-weight: 600;
+      font-family: "M PLUS Rounded 1c", sans-serif;
+      margin-bottom: 1rem;
+    }
+    .trait {
+      display: flex;
+      gap: 1rem;
+      span {
+        font-size: 1.5rem;
+        font-weight: 100;
+      }
+    }
+  }
+  .right {
+    position: relative;
+    img {
+      padding: 2px;
+      border-radius: 50%;
+      border: 1px solid var(--gray-500);
+      width: 10rem;
+      height: 10rem;
+    }
+    &:hover {
+      &::after {
+        content: 'Github Profile';
+        font-size: 1rem;
+        font-weight: 300;
+        color: var(--gray-500);
+        position: absolute;
+        text-align: center;
+        width: max-content;
+        bottom: -2rem;
+        left: 50%;
+        transform: translateX(-50%);
+      }
+    }
+  }
 `;
 
 export default Home;
