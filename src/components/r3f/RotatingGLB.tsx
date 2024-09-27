cceaa9bbbd798d51b00c990e14d61b84deb5108f
lofi-J <jsj2505@naver.com>
2024-09-27 21:52:26 +0900

    [dev] Character3D 초기 렌더시 빠른 회전

--- src/components/r3f/RotatingGLB.tsx
+++ src/components/r3f/RotatingGLB.tsx
@@ -3,13 +3,19 @@ import GLBLoader from '../../modules/GLBLoader.tsx';
 import { Group } from 'three';
 import { useFrame } from '@react-three/fiber';
 
+
 const RotatingGLB = ({url}: {url: string}) => {
   const ref = useRef<Group>(null);
   const [isDragging, setIsDragging] = useState(false);
+  let speed = 0.8;
+  
   
   useFrame(() => {
+    if (speed > 0.003) {
+      speed-= speed * 0.01;
+    }
     if (ref.current && !isDragging) {
-      ref.current.rotation.y += 0.003;
+      ref.current.rotation.y += speed;
     }
   });
   
