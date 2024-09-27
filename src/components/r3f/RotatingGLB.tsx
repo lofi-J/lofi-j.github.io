2d509a88f2e54f95d1b75c2362830fee9cc8c608
lofi-J <jsj2505@naver.com>
2024-09-27 22:06:32 +0900

    [modify] Character3D 초기 렌더시 빠른 회전
     - 오브젝트 드래깅시 re-rendering되며 speed가 초기화 되던 버그 수정
    
     - speed를 useRef로 관리

--- src/components/r3f/RotatingGLB.tsx
+++ src/components/r3f/RotatingGLB.tsx
@@ -3,19 +3,20 @@ import GLBLoader from '../../modules/GLBLoader.tsx';
 import { Group } from 'three';
 import { useFrame } from '@react-three/fiber';
 
+const ORIGIN_SPEED = 0.003;
 
 const RotatingGLB = ({url}: {url: string}) => {
   const ref = useRef<Group>(null);
+  const speedRef = useRef<number>(0.8);
   const [isDragging, setIsDragging] = useState(false);
-  let speed = 0.8;
   
   
   useFrame(() => {
-    if (speed > 0.003) {
-      speed-= speed * 0.01;
+    if (speedRef.current > ORIGIN_SPEED) {
+      speedRef.current -= speedRef.current * 0.01;
     }
     if (ref.current && !isDragging) {
-      ref.current.rotation.y += speed;
+      ref.current.rotation.y += speedRef.current;
     }
   });
   
