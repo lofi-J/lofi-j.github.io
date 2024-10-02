ad81b67aa664418137146af26485b1101eb6eaed
lofi-J <jsj2505@gmail.com>
2024-10-02 18:50:07 +0900

    [add] .nojekyll
     - vite.config.ts 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig(() => {
-  return {
-    plugins: [
-      react({ jsxImportSource: '@emotion/react' }),
-      svgr()
-    ],
-    server: {
-      port: 3000,
-      strictPort: true
-    },
-    base: '/',
-  };
+export default defineConfig({
+  plugins: [
+    react({ jsxImportSource: '@emotion/react' }),
+    svgr()
+  ],
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  build: {
+    target: 'esnext',  // 최신 JavaScript 버전으로 빌드
+    minify: 'terser',   // minify 설정
+    sourcemap: false,   // 소스맵 비활성화 (선택 사항)
+  },
 });
