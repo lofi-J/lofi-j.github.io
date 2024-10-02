6a452c9b4a7a3e3334cd90db3e13cfa4bcf7716d
lofi-J <jsj2505@gmail.com>
2024-10-02 17:57:32 +0900

    [deploy] deploy settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({jsxImportSource: '@emotion/react'}),
-    svgr()
-  ],
-  server: {
-    port: 3000,
-    strictPort: true
-  },
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react({ jsxImportSource: '@emotion/react' }),
+      svgr()
+    ],
+    server: {
+      port: 3000,
+      strictPort: true
+    },
+    base: mode === 'production' ? '/HomePage/' : '/', // 프로덕션에서는 /HomePage/ 경로, 개발 환경에서는 기본 경로
+  };
+});
